2d542b5bb712963f24d12b3fa958bd5d2ee7d228
PARTH3612 <parthchoudhari3612@gmail.com>
2024-08-23 11:03:55 +0530

    2nd commit before branch1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import AdminDashboard from "./Hospital/Dashboard/AdminDashboard";
 import { ChakraProvider } from "@chakra-ui/react";
 function App() {
   return (
-    <ChakraProvider>
+  
           <AdminDashboard/>
-    </ChakraProvider>
+
  
   );
 }
